cc0ac290c0715491608cf7cc6ae8c161d02d8146
TaniGit <TaniGit@users.noreply.github.com>
2023-08-20 13:46:43 +0900

    Renewal jjyRepeater.c & add jjyRepeater.service

--- jjyRepeater.c
+++ jjyRepeater.c
@@ -8,8 +8,11 @@
 #include <err.h>
 #include <string.h>
 #include <errno.h>
+#include <pthread.h>
 
-#define PWM_PIN 18
+#define PRG_ON_PIN  6
+#define PWM_PIN    12
+#define PWM_PIN2   13
 
 #define PWM_CLOCK_JJY40 48
 #define PWM_CLOCK_JJY60 32
@@ -22,30 +25,41 @@
 #define true  (1)
 #define false (0)
 
-#define PIDFILE ("/var/run/jjyRepeater.pid")
+#define PIDFILE ("/run/jjyRepeater.pid")
+#define NTPFILE ("/tmp/ntp_status")
 
-volatile sig_atomic_t sig_flag = 0;
+volatile sig_atomic_t sig_flag   = 0;
+
+unsigned int getNtpStatusFromFile();
 
 int init_pwm()
 {
     if (wiringPiSetupGpio() == -1) return false;
 
-    pinMode(PWM_PIN,PWM_OUTPUT);
+    pinMode(PRG_ON_PIN,OUTPUT    );
+    pinMode(PWM_PIN   ,PWM_OUTPUT);
+    pinMode(PWM_PIN2  ,PWM_OUTPUT);
 
     pwmSetMode(PWM_MODE_MS);
     pwmSetClock(PWM_CLOCK_JJY40);
     pwmSetRange(RANGE);
 
-    pwmWrite(PWM_PIN,JJY_OFF);
-
+    pwmWrite(PWM_PIN ,JJY_OFF);
+    pwmWrite(PWM_PIN2,      0);
+    digitalWrite(PRG_ON_PIN,HIGH);
 
     return true;
 }
 
 void finish()
 {
-    pwmWrite(PWM_PIN,JJY_OFF);
-    pinMode(PWM_PIN,INPUT);
+    pwmWrite(PWM_PIN ,JJY_OFF);
+    pwmWrite(PWM_PIN2,      0);
+    digitalWrite(PRG_ON_PIN,LOW);
+
+    pinMode(PRG_ON_PIN,INPUT);
+    pinMode(PWM_PIN   ,INPUT);
+    pinMode(PWM_PIN2  ,INPUT);
 }
 
 static inline void send_bit(int bit)
@@ -62,11 +76,84 @@ static inline void send_bit(int bit)
     pwmWrite(PWM_PIN,JJY_ON);
     switch(bit)
     {
-        case -1: delayMicroseconds(200000); break;
-        case  0: delayMicroseconds(800000); break;
-        case  1: delayMicroseconds(500000); break;
+        case -1: delayMicroseconds(200 * 1000);break;
+        case  0: delayMicroseconds(800 * 1000);break;
+        case  1: delayMicroseconds(500 * 1000);break;
     }
     pwmWrite(PWM_PIN,JJY_OFF);
+}
+
+unsigned int getNtpStatusFromFile()
+{
+    FILE *fp;
+    char *cmdline = "timedatectl status | grep 'System clock synchronized:' | grep 'yes' | wc -l";
+    if ((fp=popen(cmdline, "r")) == NULL) {
+        return false;
+    }
+
+    unsigned int ret;
+    while(fscanf(fp, "%d", &ret) != EOF) {}
+
+    pclose(fp);
+
+    return ret;
+}
+
+unsigned int wait_ntp_sync(unsigned int interval_sec,unsigned int limit_sec)
+{
+    unsigned int sec    = 0;
+    unsigned int on_off = 0;
+    while(!getNtpStatusFromFile())
+    {
+        if((limit_sec != 0) & (sec > limit_sec))
+	{
+            pwmWrite(PWM_PIN2,0);
+            return false;
+        }
+        for(int i=0;i<interval_sec;i++)
+        {
+            if(on_off == 0) on_off = RANGE;
+	    else            on_off = 0;
+            pwmWrite(PWM_PIN2,on_off);
+            sleep(1);
+            sec++;
+        }
+    }
+    pwmWrite(PWM_PIN2,0);
+    return true;
+}
+
+void *ntp_monitor(void *arg)
+{
+    unsigned int times    = 0;
+    unsigned int p2_width = 0;
+    unsigned int status;
+    int up = true;
+
+    status = getNtpStatusFromFile();
+    while(true)
+    {
+        if (status)
+        {
+            pwmWrite(PWM_PIN2,p2_width);
+            if(up) p2_width++;
+            else   p2_width--;
+            if(p2_width == 0     ) up = true;
+            if(p2_width == RANGE ) up = false;
+            usleep(100 * 1000);
+            times++;
+            if(times == 600)
+            {
+                status = getNtpStatusFromFile();
+	        times = 0;
+            }
+        } else {
+	    status = wait_ntp_sync(30,0);
+            times = 0;
+        }
+    }
+    pwmWrite(PWM_PIN2,      0);
+    pthread_exit(NULL);
 }
 
 void mkjjyCode(int *jjyCode)
@@ -119,10 +206,10 @@ void mkjjyCode(int *jjyCode)
     jjyCode[38] = 0;
     jjyCode[39] = -1;
     jjyCode[40] = 0;
-    jjyCode[41] = ((((now_st->tm_year % 100) / 10) & 0x8) >> 3);
-    jjyCode[42] = ((((now_st->tm_year % 100) / 10) & 0x4) >> 2);
-    jjyCode[43] = ((((now_st->tm_year % 100) / 10) & 0x2) >> 1);
-    jjyCode[44] =  (((now_st->tm_year % 100) / 10) & 0x1);
+    jjyCode[41] = ((((now_st->tm_year / 10) % 10) & 0x8) >> 3);
+    jjyCode[42] = ((((now_st->tm_year / 10) % 10) & 0x4) >> 2);
+    jjyCode[43] = ((((now_st->tm_year / 10) % 10) & 0x2) >> 1);
+    jjyCode[44] =  (((now_st->tm_year / 10) % 10) & 0x1);
     jjyCode[45] = (((now_st->tm_year  % 10) & 0x8) >> 3);
     jjyCode[46] = (((now_st->tm_year  % 10) & 0x4) >> 2);
     jjyCode[47] = (((now_st->tm_year  % 10) & 0x2) >> 1);
@@ -166,15 +253,26 @@ void getsignal(int sig)
     switch(sig)
     {
         case SIGINT:
-        case SIGTERM:
         case SIGQUIT:
+        //case SIGKILL:
+        case SIGTERM:
+            sig_flag = 6;
+            break;
+        case SIGHUP:
+            sig_flag = 5;
+            break;
+        //case SIGSTOP:
+        case SIGTSTP:
             sig_flag = 4;
             break;
-        case SIGTSTP:
+        case SIGCONT:
             sig_flag = 3;
             break;
-        case SIGCONT:
+        case SIGUSR1:
             sig_flag = 2;
+            break;
+        case SIGUSR2:
+            sig_flag = 1;
             break;
      }
 }
@@ -199,6 +297,8 @@ int main(int argc,char *argv[])
     int jjyCode[60];
     unsigned int i,wait_time;
 
+    unsigned int is_running = true;
+
     struct sigaction act;
 
     struct timeval now_u;
@@ -206,55 +306,79 @@ int main(int argc,char *argv[])
 
     const char *pid_file = PIDFILE;
 
+    pthread_t ntp_mon;
+
     memset(&act,0,sizeof(act));
     act.sa_handler = getsignal;
     act.sa_flags   = SA_RESETHAND;
 
-
-    if(getuid() != 0)                     err(1,"Require ROOT!\n");
+    if(getuid() != 0)                     err( 1,"Require ROOT!\n");
     jjyProcCheck();
+    if(sigaction(SIGHUP ,&act,NULL) != 0) err(-1,"Error: sigaction(SIGINT)" );
     if(sigaction(SIGINT ,&act,NULL) != 0) err(-1,"Error: sigaction(SIGINT)" );
-    if(sigaction(SIGTERM,&act,NULL) != 0) err(-1,"Error: sigaction(SIGTERM)");
     if(sigaction(SIGQUIT,&act,NULL) != 0) err(-1,"Error: sigaction(SIGQUIT)");
+    //if(sigaction(SIGKILL,&act,NULL) != 0) err(-1,"Error: sigaction(SIGTERM)");
+    if(sigaction(SIGTERM,&act,NULL) != 0) err(-1,"Error: sigaction(SIGTERM)");
+    //if(sigaction(SIGSTOP,&act,NULL) != 0) err(-1,"Error: sigaction(SIGTSTP)");
     if(sigaction(SIGTSTP,&act,NULL) != 0) err(-1,"Error: sigaction(SIGTSTP)");
     if(sigaction(SIGCONT,&act,NULL) != 0) err(-1,"Error: sigaction(SIGCOUT)");
-    if(sigaction(SIGHUP ,&act,NULL) != 0) err(-1,"Error: sigaction(SIGHUP)" );
-
+    if(sigaction(SIGUSR1,&act,NULL) != 0) err(-1,"Error: sigaction(SIGHUP)" );
+    if(sigaction(SIGUSR2,&act,NULL) != 0) err(-1,"Error: sigaction(SIGHUP)" );
 
     if(!init_pwm())        errno = ENOTTY,err(-1,"pwm_init() error");
 
-    while(sig_flag < 4)
+    wait_ntp_sync(10,180);
+
+    if(pthread_create(&ntp_mon,NULL,ntp_monitor,NULL) != 0)
+                           errno = ENOTTY,err(-1,"ntp_monitor() error");
+
+    while(sig_flag < 6)
     {
-
-        if(sig_flag == 3){
-            kill(getpid(),SIGTSTP);
-        }
-
-        if(sig_flag == 2)
+        switch(sig_flag)
         {
-
-            kill(getpid(),SIGCONT);
-
-            if(sigaction(SIGTSTP,&act,NULL) != 0)
-            {
+            case 5:
+                pthread_cancel(ntp_mon);
+                pthread_join(ntp_mon,NULL);
                 finish();
-                err(-2,"Error: set sigaction(SIGTSTP)");
-            }
-
-            if(sigaction(SIGCONT,&act,NULL) != 0)
-            {
-                finish();
-                err(-2,"Error: set sigaction(SIGCONT)");
-            }
-
-            sig_flag = 0;
+                if(!init_pwm()) errno = ENOTTY,err(-1,"pwm_init() error");
+                if(pthread_create(&ntp_mon,NULL,ntp_monitor,NULL) != 0)
+                                errno = ENOTTY,err(-1,"ntp_monitor() error");
+                sig_flag = 0;
+                break;
+            case 4:
+                if(is_running)
+                {
+                    pthread_cancel(ntp_mon);
+                    pthread_join(ntp_mon,NULL);
+                    is_running = false;
+                }
+                usleep(100 * 1000);
+                break;
+            case 3:
+                if(!is_running)
+                {
+                    if(pthread_create(&ntp_mon,NULL,ntp_monitor,NULL) != 0)
+                        errno = ENOTTY,err(-1,"ntp_monitor() error");
+                    is_running = true;
+                }
+                sig_flag = 0;
+                break;
+            case 2:
+                sig_flag = 0;
+                break;
+            case 1:
+                sig_flag = 0;
+                break;
+            default:
+               wait_start();
+               mkjjyCode(jjyCode);
+               for(i=0;i<60;i++) send_bit(jjyCode[i]);
+               break;
         }
-
-        wait_start();
-        mkjjyCode(jjyCode);
-        for(i=0;i<60;i++) send_bit(jjyCode[i]);
     }
 
+    pthread_cancel(ntp_mon);
+    pthread_join(ntp_mon,NULL);
     finish();
 
     if(remove(pid_file) != 0) err(1,"Could not Remove PID file");
